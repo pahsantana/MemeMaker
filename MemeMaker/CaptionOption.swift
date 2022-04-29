f1690ab0feb5c8101dbfea59236a97884a6b68bd
Paloma Santana <santanagilberto@uol.com.br>
2022-04-29 00:25:36 -0300

    memeMaker
    Projeto desenvolvido que tem como objetivo clicar em diferentes segmentedControls para obter frases engraçadas (memes) durante o treinamento Swift Explorations

---
+++ MemeMaker/CaptionOption.swift
@@ -0,0 +1,13 @@
+//
+//  CaptionOption.swift
+//  MemeMaker
+//
+//  Created by Paloma Cristina Santana on 19/04/22.
+//
+
+import Foundation
+
+struct CaptionOption {
+    let emoji: String
+    let caption: String
+}
--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -8,12 +8,91 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var topSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var bottomSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var topCationLabel: UILabel!
+    
+    @IBOutlet weak var bottomCaptionLabel: UILabel!
+    
+    let 🥰 = CaptionOption(emoji: "🥰", caption: "O que eu preciso na minha vida")
+    
+    let 👸🏻 = CaptionOption(emoji: "👸🏻", caption: "Ser herdeira do trono de Genovia")
+    
+    let 🤑 = CaptionOption(emoji: "🤑", caption: "Aceita pix nada")
+    
+    let 🍬 = CaptionOption(emoji: "🍬", caption: "Sou da época que aceita bala como troco")
+    
+    let 🏠 = CaptionOption(emoji: "🏠", caption: "Você é proprietário de imóvel")
+    
+    let 🔑 = CaptionOption(emoji: "🔑", caption: "Que tal ceder as chaves para mim")
+    
+    
+    var topChoices = [CaptionOption]()
+    var bottomChoices = [CaptionOption]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        topSegmentedControl.removeAllSegments()
+        bottomSegmentedControl.removeAllSegments()
         // Do any additional setup after loading the view.
+        
+        let topChoices = [🥰,🤑,🏠]
+        
+        let bottomChoices = [👸🏻,🍬,🔑]
+        
+        topSegmentedControl.selectedSegmentIndex = 0
+        bottomSegmentedControl.selectedSegmentIndex = 0
+        
+        insertSegments(choices: topChoices, control: topSegmentedControl)
+        insertSegments(choices: bottomChoices, control: bottomSegmentedControl)
+    }
+    
+    func insertSegments(choices: [CaptionOption], control: UISegmentedControl){
+        for choice in choices {
+            control.insertSegment(withTitle: choice.emoji, at: choices.count, animated: false)
+        }
     }
 
-
+    @IBAction func actionSegmentedControl(_ sender: UISegmentedControl) {
+        
+        switch sender.titleForSegment(at: sender.selectedSegmentIndex){
+            case "🥰":
+                topCationLabel.text = 🥰.caption
+            case "🤑":
+                topCationLabel.text = 🤑.caption
+            
+            case "🏠":
+                topCationLabel.text = 🏠.caption
+            
+            case "👸🏻":
+                bottomCaptionLabel.text = 👸🏻.caption
+            
+            case "🔑":
+                bottomCaptionLabel.text = 🔑.caption
+            
+            case "🍬":
+            bottomCaptionLabel.text = 🍬.caption
+            
+            default:
+                
+                fatalError()
+        }
+    }
+    
+    @IBAction func dragTopLabel(_ sender: UIPanGestureRecognizer) {
+        
+        if sender.state == .changed{
+            topCationLabel.center = sender.location(in: view)
+        }
+    }
+    @IBAction func dragBottomLabel(_ sender: UIPanGestureRecognizer) {
+        
+        if sender.state == .changed{
+            bottomCaptionLabel.center = sender.location(in: view)
+        }
+    }
 }
 
